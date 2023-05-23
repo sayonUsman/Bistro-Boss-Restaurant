962466033487a7d48919380028bcafc6c48d1197
Sayon Usman <sayonusman.ma@gmail.com>
2023-05-23 22:19:11 +0300

    Added order menu

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 import Banner from "./Banner/Banner";
+import OrderMenu from "./OrderMenu/OrderMenu";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+
+      <div className="mb-10">
+        <OrderMenu></OrderMenu>
+      </div>
     </div>
   );
 };
---
+++ src/pages/Home/OrderMenu/OrderMenu.jsx
@@ -0,0 +1,91 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+import slide1 from "../../../assets/orderMenu//slide1.jpg";
+import slide2 from "../../../assets/orderMenu//slide2.jpg";
+import slide3 from "../../../assets/orderMenu//slide3.jpg";
+import slide4 from "../../../assets/orderMenu//slide4.jpg";
+
+const OrderMenu = () => {
+  return (
+    <>
+      <Swiper
+        slidesPerView={5}
+        spaceBetween={25}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            src={slide1}
+            alt="Menu Item"
+            className="w-full rounded-md lg:rounded-xl"
+          ></img>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img
+            src={slide2}
+            alt="Menu Item"
+            className="w-full rounded-md lg:rounded-xl"
+          ></img>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img
+            src={slide3}
+            alt="Menu Item"
+            className="w-full rounded-md lg:rounded-xl"
+          ></img>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img
+            src={slide4}
+            alt="Menu Item"
+            className="w-full rounded-md lg:rounded-xl"
+          ></img>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img
+            src={slide1}
+            alt="Menu Item"
+            className="w-full rounded-md lg:rounded-xl"
+          ></img>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img
+            src={slide2}
+            alt="Menu Item"
+            className="w-full rounded-md lg:rounded-xl"
+          ></img>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img
+            src={slide3}
+            alt="Menu Item"
+            className="w-full rounded-md lg:rounded-xl"
+          ></img>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img
+            src={slide4}
+            alt="Menu Item"
+            className="w-full rounded-md lg:rounded-xl"
+          ></img>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default OrderMenu;
