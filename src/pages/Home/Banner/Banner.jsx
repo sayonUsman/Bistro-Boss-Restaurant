8becfefef8a2551224629f6a2d15078fc5a436bc
Sayon Usman <sayonusman.ma@gmail.com>
2023-05-23 20:22:29 +0300

    Added code to auto play banner slides

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -9,29 +9,29 @@ import slide6 from "../../../assets/banner/slide-06.png";
 
 const Banner = () => {
   return (
-    <Carousel>
+    <Carousel autoPlay infiniteLoop interval={4000}>
       <div>
-        <img src={slide1} />
+        <img src={slide1} alt="Slider Image" />
       </div>
 
       <div>
-        <img src={slide2} />
+        <img src={slide2} alt="Slider Image" />
       </div>
 
       <div>
-        <img src={slide3} />
+        <img src={slide3} alt="Slider Image" />
       </div>
 
       <div>
-        <img src={slide4} />
+        <img src={slide4} alt="Slider Image" />
       </div>
 
       <div>
-        <img src={slide5} />
+        <img src={slide5} alt="Slider Image" />
       </div>
 
       <div>
-        <img src={slide6} />
+        <img src={slide6} alt="Slider Image" />
       </div>
     </Carousel>
   );
