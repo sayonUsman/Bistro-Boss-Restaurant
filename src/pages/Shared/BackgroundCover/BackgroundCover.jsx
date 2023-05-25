b617bcab69e3c6d4934cc4cfbae59c018ca18d2f
Sayon Usman <sayonusman.ma@gmail.com>
2023-05-25 15:19:56 +0300

    UI design of background cover has been update

--- src/pages/Shared/BackgroundCover/BackgroundCover.jsx
+++ src/pages/Shared/BackgroundCover/BackgroundCover.jsx
@@ -6,11 +6,15 @@ const BackgroundCover = ({ img }) => {
         backgroundImage: `url(${img})`,
       }}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="w-9/12 h-48 lg:h-72 hero-overlay bg-opacity-60"></div>
+
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">OUR MENU</h1>
-          <p className="mb-5">WOULD YOU LIKE TO TRY A DISH?</p>
+          <h1 className="mb-2 lg:mb-4 text-4xl lg:text-5xl font-bold">
+            OUR MENU
+          </h1>
+
+          <p className="mb-5 text-sm">WOULD YOU LIKE TO TRY A DISH?</p>
         </div>
       </div>
     </div>
