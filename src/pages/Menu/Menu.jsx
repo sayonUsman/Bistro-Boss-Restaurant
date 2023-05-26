d75d5572f17daac0156b5e773af94d68b7d7e2b4
Sayon Usman <sayonusman.ma@gmail.com>
2023-05-26 16:26:47 +0300

    Added menu category in menu page

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,6 +1,11 @@
 import { Helmet } from "react-helmet-async";
 import BackgroundCover from "../Shared/BackgroundCover/BackgroundCover";
 import banner from "../../assets/menu/banner.jpg";
+import dessert from "../../assets/menu/dessert-bg.jpeg";
+import pizza from "../../assets/menu/pizza-bg.jpg";
+import salad from "../../assets/menu/salad-bg.jpg";
+import soup from "../../assets/menu/soup-bg.jpg";
+import MenuCategory from "./MenuCategory/MenuCategory";
 
 const Menu = () => {
   return (
@@ -14,6 +19,38 @@ const Menu = () => {
         title={"OUR MENU"}
         subTitle={"WOULD YOU LIKE TO TRY A DISH?"}
       ></BackgroundCover>
+
+      <MenuCategory
+        img={dessert}
+        title={"DESSERTS"}
+        subTitle={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+      ></MenuCategory>
+
+      <MenuCategory
+        img={pizza}
+        title={"PIZZA"}
+        subTitle={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+      ></MenuCategory>
+
+      <MenuCategory
+        img={salad}
+        title={"SALADS"}
+        subTitle={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+      ></MenuCategory>
+
+      <MenuCategory
+        img={soup}
+        title={"SOUPS"}
+        subTitle={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+      ></MenuCategory>
     </div>
   );
 };
---
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -0,0 +1,24 @@
+const MenuCategory = ({ img, title, subTitle }) => {
+  return (
+    <div
+      className="hero h-[625px]"
+      style={{
+        backgroundImage: `url(${img})`,
+      }}
+    >
+      <div className="w-9/12 h-48 lg:h-72 hero-overlay bg-opacity-60"></div>
+
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-2 lg:mb-4 text-4xl lg:text-5xl font-bold">
+            {title}
+          </h1>
+
+          <p className="mb-5 text-sm">{subTitle}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MenuCategory;
