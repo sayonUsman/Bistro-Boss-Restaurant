cd2626114b9a4d52f10782aaddb5868801366e8d
Sayon Usman <sayonusman.ma@gmail.com>
2023-05-27 20:17:05 +0300

    Added price in item card

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -5,12 +5,16 @@ const ItemCard = ({ item }) => {
         <img src={item.image} alt={item.name} className="w-full h-72" />
       </figure>
 
+      <p className="absolute right-3 top-3 rounded pl-3 pr-3 bg-black bg-opacity-60 text-white">
+        ${item.price}
+      </p>
+
       <div className="card-body">
         <h2 className="card-title">{item.name}</h2>
         <p className="text-left">{item.descriptions.slice(0, 125) + "..."}</p>
 
         <div className="card-actions justify-end">
-          <button className="btn">Order Now</button>
+          <button className="btn">Add To Cart</button>
         </div>
       </div>
     </div>
