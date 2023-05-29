2bfc9fb26104d29f926226c28ca379a31ffa5058
Sayon Usman <sayonusman.ma@gmail.com>
2023-05-29 21:10:35 +0300

    Added context API

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,12 +5,19 @@ import {
   LoadCanvasTemplate,
   validateCaptcha,
 } from "react-simple-captcha";
-import { useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useContext, useRef, useState } from "react";
+import { AuthContext } from "../../authProvider/AuthProvider";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Login = () => {
   useEffect(() => loadCaptchaEnginge(6), []);
+  const { loginWithEmailAndPassword } = useContext(AuthContext);
   const captchaRef = useRef(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const [isDisabled, SetIsDisabled] = useState(true);
 
   const verifyCaptcha = () => {
@@ -28,117 +35,149 @@ const Login = () => {
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
-    form.reset();
+
+    setMessage("");
+    setErrorMessage("");
+
+    loginWithEmailAndPassword(email, password)
+      .then(() => {
+        form.reset();
+        setMessage("Successfully signed in");
+        navigate(from, { replace: true });
+        SetIsDisabled(true);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+        SetIsDisabled(true);
+      });
   };
 
   return (
-    <div>
-      <div
-        className="hero min-h-screen bg-fixed"
-        style={{
-          backgroundImage: `url(${bg})`,
-        }}
-      >
-        <div className="hero-content flex-col">
-          <div className="hero w-9/12 lg:w-[925px] rounded-lg shadow-lg shadow-zinc-900">
-            <div className="hero-content flex-col lg:flex-row">
-              <div className="lg:w-9/12">
-                <img src={authImg} alt="Image" />
+    <div
+      className="hero min-h-screen bg-fixed"
+      style={{
+        backgroundImage: `url(${bg})`,
+      }}
+    >
+      <div className="hero-content flex-col">
+        <div className="hero w-9/12 lg:w-[925px] rounded-lg shadow-lg shadow-zinc-900">
+          <div className="hero-content flex-col lg:flex-row">
+            <div className="lg:w-9/12">
+              <img src={authImg} alt="Image" />
+            </div>
+
+            <form
+              className="card rounded-lg flex-shrink-0 w-full max-w-sm"
+              onSubmit={handleSubmit}
+            >
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+
+                <input
+                  type="email"
+                  placeholder="email"
+                  id="email"
+                  required
+                  className="input input-bordered"
+                />
               </div>
 
-              <form
-                className="card rounded-lg flex-shrink-0 w-full max-w-sm"
-                onSubmit={handleSubmit}
-              >
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+
+                <input
+                  type="password"
+                  placeholder="password"
+                  id="password"
+                  required
+                  className="input input-bordered"
+                />
+
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+
+              <div className="form-control">
+                <label className="label mb-3">
+                  <LoadCanvasTemplate />
+                </label>
+
+                <input
+                  type="text"
+                  ref={captchaRef}
+                  placeholder="Type the captcha here"
+                  id="captcha"
+                  required
+                  className="input input-bordered"
+                />
+
+                {isDisabled && (
+                  <label className="label label-text-alt">
+                    To Verify Your Captcha
+                    <button
+                      className="link link-hover text-red-500"
+                      onClick={() => verifyCaptcha()}
+                    >
+                      Click Here
+                    </button>
                   </label>
+                )}
+              </div>
 
-                  <input
-                    type="email"
-                    placeholder="email"
-                    id="email"
-                    required
-                    className="input input-bordered"
-                  />
-                </div>
-
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-
-                  <input
-                    type="password"
-                    placeholder="password"
-                    id="password"
-                    required
-                    className="input input-bordered"
-                  />
-
-                  <label className="label">
-                    <a href="#" className="label-text-alt link link-hover">
-                      Forgot password?
-                    </a>
-                  </label>
-                </div>
-
-                <div className="form-control">
-                  <label className="label mb-3">
-                    <LoadCanvasTemplate />
-                  </label>
-
-                  <input
-                    type="text"
-                    ref={captchaRef}
-                    placeholder="Type the captcha here"
-                    id="captcha"
-                    required
-                    className="input input-bordered"
-                  />
-
-                  {isDisabled && (
-                    <label className="label label-text-alt">
-                      To Verify Your Captcha
-                      <button
-                        className="link link-hover text-red-500"
-                        onClick={() => verifyCaptcha()}
-                      >
-                        Click Here
-                      </button>
-                    </label>
-                  )}
-                </div>
-
-                <div className="form-control mt-10">
-                  <input
-                    className="btn bg-[#D1A054]"
-                    type="submit"
-                    value="Login"
-                    disabled={isDisabled}
-                  />
-                </div>
-              </form>
-            </div>
-          </div>
-
-          <div className="text-center mt-10 lg:mt-14">
-            <p className="mb-2">
-              New here?{" "}
-              <span>
-                <Link to="/signUp" className="link link-hover">
-                  Create a New Account
-                </Link>
-              </span>
-            </p>
-
-            <Link to="/" className="link link-hover">
-              Back to Home
-            </Link>
+              <div className="form-control mt-10">
+                <input
+                  className="btn bg-[#D1A054]"
+                  type="submit"
+                  value="Login"
+                  disabled={isDisabled}
+                />
+              </div>
+            </form>
           </div>
         </div>
+
+        <div className="text-center mt-10 lg:mt-14">
+          <p className="mb-2">
+            New here?{" "}
+            <span>
+              <Link to="/signUp" className="link link-hover">
+                Create a New Account
+              </Link>
+            </span>
+          </p>
+
+          <Link to="/" className="link link-hover">
+            Back to Home
+          </Link>
+        </div>
       </div>
+
+      {message && (
+        <div className="toast toast-end">
+          <div className="alert alert-success">
+            <div>
+              <span>{message}</span>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {errorMessage && (
+        <div className="toast toast-end">
+          <div className="alert alert-error">
+            <div>
+              <span>{errorMessage}</span>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
